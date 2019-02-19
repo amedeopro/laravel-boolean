423cfbb515a76087c765f2b667df33b46d77b1ba
amedeopro <amedeopro@me.com>
2019-02-19 16:54:53 +0100

    footer ok solo da stilare

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,27 +1,27 @@
 <div class="pre-footer">
   <div class="container">
     <div class="box">
-      <div class="col-3">
+      
         <img src="{{ asset('img/logo.png')}}" alt="">
-      </div>
+      
     </div>
     <div class="box">
-      <div class="col-3">
+      
         <p>ciao@boolean.careers</p>
         <p>02-40031288</p>
         <p>+39 327-9578239</p>
-      </div>
+      
     </div>
     <div class="box">
-      <div class="col-3">
+      
         <a href="{{ route('privacy')}}">Privacy Policy</a>
         <a href="{{ route('lavora-con-noi')}}">Lavora con noi</a>
-      </div>
+      
     </div>
     <div class="box">
-      <div class="col-3">
+      
         <p>icona1 - icona2 - icona3</p>
-      </div>
+      
     </div>
   </div>
 </div>
@@ -35,7 +35,7 @@
     <div class="footer-right">
 
         <p>Boolean SRLS - Piazzale Giovanni dalle Bande Nere, 9 - 20146, Milano - P.IVA: 10214930967</p>
-      
+
 
     </div>
   </div>
