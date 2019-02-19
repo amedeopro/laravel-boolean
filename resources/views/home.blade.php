7bf31efeebc23ce9b3b0ae1270bdd8658f817575
amedeopro <amedeopro@me.com>
2019-02-19 09:24:58 +0100

    .

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="it" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Boolean</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+  </head>
+  <body>
+    <div class="header-left">
+      <img src="{{ asset('img/logo.png')}}" alt="">
+    </div>
+    <div class="header-right">
+      <ul>
+        <li><a href="{{ route('home') }}">home</a></li>
+        <li><a href="#">corso</a></li>
+        <li><a href="#">metodo</a></li>
+        <li><a href="#">dopo il corso</a></li>
+        <li><a href="#">faq</a></li>
+        <li><a href="#" class="orange">iscriviti</a></li>
+      </ul>
+    </div>
+  </body>
+</html>
